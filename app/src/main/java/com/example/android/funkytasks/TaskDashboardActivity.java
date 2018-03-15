14d70d18aa6493e72f80a7dfdb2377d4ce322f8f
f30jimi <shanghaidawang@gmail.com>
2018-03-14 21:42:21 -0600

    5.04.1 working US 05.05.01-US 05.07.01 front end done,waiting for E.S

--- app/src/main/java/com/example/android/funkytasks/TaskDashboardActivity.java
+++ app/src/main/java/com/example/android/funkytasks/TaskDashboardActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,10 +16,14 @@ import java.util.ArrayList;
 public class TaskDashboardActivity extends AppCompatActivity {
     ArrayList<User> userArrayList = new ArrayList<User>();
     private String username;
+    CheckBox statusCheckbox;
 
     ListView listView;
     ListViewAdapter listViewAdapter;
+    ListViewAdapter listViewAdapter1;
     ArrayList<Task> taskList = new ArrayList<Task>();
+    ArrayList<Task> biddedTaskList = new ArrayList<Task>();
+
 
     final int DELETECODE = 1;
     ArrayList<Task> requestedTasks;
@@ -30,6 +36,7 @@ public class TaskDashboardActivity extends AppCompatActivity {
         Intent intent = getIntent();
         username = intent.getExtras().getString("username");
         username = LoginActivity.username;
+        statusCheckbox=(CheckBox) findViewById(R.id.checkBox);
 
         ElasticSearchController.GetUser getUser = new ElasticSearchController.GetUser();
         getUser.execute(username);
@@ -54,19 +61,49 @@ public class TaskDashboardActivity extends AppCompatActivity {
             return;
         }
 
+        int size=taskList.size();
+        for(int i=0;i<size;i++){
+            if(taskList.get(i).getStatus()=="bidded"){
+                biddedTaskList.add(taskList.get(i));
+            }
+        }
+
+
+
 
         listView = (ListView) findViewById(R.id.myTasks);
         listViewAdapter = new ListViewAdapter(this, R.layout.listviewitem, taskList);
-
+        listViewAdapter1 = new ListViewAdapter(this, R.layout.listviewitem, biddedTaskList);
         listView.setAdapter(listViewAdapter);
         listViewAdapter.notifyDataSetChanged(); //TODO fix the delete functionality (works backend wise) but is not reflected in front end after
+
+       //show bided task
+        statusCheckbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(statusCheckbox.isChecked()){
+                    listView.setAdapter(listViewAdapter1);
+                }
+                else{
+                    listView.setAdapter(listViewAdapter);
+                }
+            }
+        });
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(TaskDashboardActivity.this, DashboardRequestedTask.class);
                 intent.putExtra("username", username);
-                Task detailedTask = taskList.get(i);
+                Task detailedTask;
+
+                if(statusCheckbox.isChecked()){
+                    detailedTask = biddedTaskList.get(i);
+                }
+                else{
+                    detailedTask = taskList.get(i);
+                }
 
                 ElasticSearchController.GetTask getTask = new ElasticSearchController.GetTask();
 
@@ -84,6 +121,8 @@ public class TaskDashboardActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
 
     }
 
