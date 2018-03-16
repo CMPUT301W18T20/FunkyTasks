0976d35882e25ad6b4d380191eb3e8dd0f1ef309
f30jimi <shanghaidawang@gmail.com>
2018-03-16 15:05:37 -0600

    TaskDashboardActivity updated

---
+++ app/src/androidTest/java/com/example/android/funkytasks/CreateTaskTest.java
@@ -0,0 +1,28 @@
+package com.example.android.funkytasks;
+
+import android.support.test.filters.SmallTest;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by jimi on 2018-03-15.
+ */
+
+public class CreateTaskTest extends ActivityInstrumentationTestCase2<CreateTaskActivity> {
+    public CreateTaskTest(Class<CreateTaskActivity> activityClass) {
+        super(activityClass);
+
+
+    }
+    @Override
+    protected void setUp(){
+
+    }
+    @SmallTest
+    public void testAddButton(){
+
+    }
+}
+
+
+
+
