8460551ac2c96197d48d55e6a2f515d7d280f506
f30jimi <shanghaidawang@gmail.com>
2018-03-16 17:41:52 -0600

    TaskDashboardActivity updated

---
+++ app/src/main/java/com/example/android/funkytasks/MyRequestedTasks.java
@@ -0,0 +1,26 @@
+package com.example.android.funkytasks;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MyRequestedTasks extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_task_dashboard, container, false);
+    return rootView;
+
+    }
+
+}
+
