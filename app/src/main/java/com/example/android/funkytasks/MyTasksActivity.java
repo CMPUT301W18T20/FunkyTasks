36553dda505cc0a94579ed5e3adf81dc5be321bb
f30jimi <shanghaidawang@gmail.com>
2018-03-16 20:28:20 -0600

    fix tab

--- app/src/main/java/com/example/android/funkytasks/MyTasksActivity.java
+++ app/src/main/java/com/example/android/funkytasks/MyTasksActivity.java
@@ -56,14 +56,6 @@ public class MyTasksActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     }
 
