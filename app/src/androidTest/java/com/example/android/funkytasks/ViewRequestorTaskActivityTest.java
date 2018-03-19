6dd2ef512f852a050b86d6f539547f73d23c8f97
Fangting Chen <fc1@ualberta.ca>
2018-03-19 13:55:56 -0600

    checks user for tests

--- app/src/androidTest/java/com/example/android/funkytasks/ViewRequestorTaskActivityTest.java
+++ app/src/androidTest/java/com/example/android/funkytasks/ViewRequestorTaskActivityTest.java
@@ -1,12 +1,15 @@
 package com.example.android.funkytasks;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import android.widget.EditText;
 
 import com.robotium.solo.Solo;
 
 import org.junit.After;
 import org.junit.Before;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -15,13 +18,35 @@ import static org.junit.Assert.*;
  */
 public class ViewRequestorTaskActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
     private Solo solo;
+    private User user;
 
     public ViewRequestorTaskActivityTest(){
         super(LoginActivity.class);
     }
 
+    //checks if user exists, if it doesnt post the user
+    public void addUser(){
+        user = new User("qwerty123", "123@gmail.com", "1112221111");
+        ElasticSearchController.GetAllUsers allUsers = new ElasticSearchController.GetAllUsers();
+        allUsers.execute(); // grab all current users in the system
+        ArrayList<User> userList = new ArrayList<User>();
+        try {
+            userList = allUsers.get();
+        } catch (Exception e) {
+            Log.e("Error", "Failed to get list of users");
+        }
+        for (User postedUser : userList) {
+            Log.e("ALl usernames", postedUser.getUsername());
+            if (postedUser.getUsername().equals(user.getUsername())) {
+                return;
+            }
+        }
+        ElasticSearchController.PostUser postUser = new ElasticSearchController.PostUser();
+        postUser.execute(user);
+    }
     public void goToViewRequeestedTask(){
         solo.assertCurrentActivity("Wrong activity", LoginActivity.class);
+        addUser();
         solo.enterText((EditText) solo.getView(R.id.editLoginName), "qwerty123");
         solo.clickOnButton("Login");
         solo.waitForActivity("MainMenuActivity.class");
