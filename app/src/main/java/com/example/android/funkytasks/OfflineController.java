c2fbe19f5cbfd84ed6990fd38674e448083289f2
Fangting Chen <fc1@ualberta.ca>
2018-03-29 16:06:52 -0600

    Saving for offline

--- app/src/main/java/com/example/android/funkytasks/OfflineController.java
+++ app/src/main/java/com/example/android/funkytasks/OfflineController.java
@@ -1,17 +1,44 @@
 package com.example.android.funkytasks;
 
+import android.content.Context;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 /**
- * Created by fc1}, Ivy on 2018-03-29.
+ * Created by fc1, Ivy on 2018-03-29.
  */
 
 public class OfflineController {
 
     private String FILENAME;
+    private Context context;
 
-    public OfflineController(String userName){
+    public OfflineController(Context content, String userName) {
         FILENAME = userName + ".sav";
     }
+
+    public void saveInFile(Task task) {
+        try {
+            FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+            Gson gson = new Gson();
+            gson.toJson(task, out);
+            out.flush();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
