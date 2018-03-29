54ebd99a9df63f226fe8a99141b5cf7d49391ef3
Eivenlour David <eivenlou@ualberta.ca>
2018-03-29 16:32:18 -0600

    Loading From File for Offline

--- app/src/main/java/com/example/android/funkytasks/OfflineController.java
+++ app/src/main/java/com/example/android/funkytasks/OfflineController.java
@@ -5,11 +5,17 @@ import android.content.Context;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Queue;
 
 /**
  * Created by fc1, Ivy on 2018-03-29.
@@ -19,6 +25,7 @@ public class OfflineController {
 
     private String FILENAME;
     private Context context;
+    private Queue<Task> taskQueue;
 
     public OfflineController(Context content, String userName) {
         FILENAME = userName + ".sav";
@@ -37,6 +44,30 @@ public class OfflineController {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    public Queue<Task> loadFromFile() {
+
+        try {
+            FileInputStream fis = context.openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+            Type dataType = new TypeToken<Queue<Task>>() {}.getType();
+
+            taskQueue = gson.fromJson(in, dataType);
+
+            return taskQueue;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+
+            throw new RuntimeException();
         }
     }
 
