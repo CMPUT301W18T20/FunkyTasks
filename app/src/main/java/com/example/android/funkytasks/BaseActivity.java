0d0d1462ca67e80d23d2132efe14a9176494c07b
Eivenlour David <eivenlou@ualberta.ca>
2018-03-31 14:44:48 -0600

    Code clean-up

--- app/src/main/java/com/example/android/funkytasks/BaseActivity.java
+++ app/src/main/java/com/example/android/funkytasks/BaseActivity.java
@@ -13,7 +13,9 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-
+/**
+ * Created by eivenlour on 2018-03-30.
+ */
 // https://stackoverflow.com/questions/10350449/how-to-check-the-internet-connection-periodically-in-whole-application
 
 public class BaseActivity extends AppCompatActivity {
